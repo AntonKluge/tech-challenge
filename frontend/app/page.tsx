99fa98147bacdd4d94c5805020c8353287afb8e0
Anton Wyrowski <anton.wyrowski@gmail.com>
2024-05-10 19:35:39 +0200

    feat: added `react-dropzone` lib

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -25,22 +25,6 @@ export default function Home() {
           Happy shopping and happy planet-saving! 🌍 📸🛍️👗👕
         </AlertDescription>
       </Alert>
-
-      <div className="flex items-center justify-center w-full">
-        <label className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-          <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            <UploadCloudIcon className="w-12 h-12 text-gray-400 dark:text-gray-500 mb-2" />
-            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span className="font-semibold">Click to upload</span> or drag and
-              drop
-            </p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">
-              Only Image files are supported
-            </p>
-          </div>
-          <input id="dropzone-file" type="file" className="hidden" />
-        </label>
-      </div>
     </main>
   );
 }
